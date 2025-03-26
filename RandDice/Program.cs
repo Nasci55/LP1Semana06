fc7b7bfedfb9e145870ed79ab4b5380cf7fcc772
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-26 22:18:02 +0000

    Finish RandDice

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -7,20 +7,14 @@ namespace RandDice
         private static void Main(string[] args)
         {
             Random random = new Random(int.Parse(args[1]));
-
             int n = 0;
-
             int maxValue = 0;
-
-
             while (n <= int.Parse(args[0]))
             {
                 maxValue += random.Next(1, 6);
                 n++;
             }
-
             System.Console.WriteLine(maxValue);
-
         }
     }
 }
