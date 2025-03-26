98edc7c44fb18f8cba4b8529cbde0dca3f43161f
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-26 22:55:07 +0000

    Add TakeDamage Method

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -23,5 +23,21 @@ namespace MyGame
         {
             return name;
         }
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+
+
+
+
+        }
     }
 }
