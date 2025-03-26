253bddb4452f56fb1525591b4f72ee3706d2039d
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-26 21:57:20 +0000

    Accept args from the console

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -6,7 +6,21 @@ namespace RandDice
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Random random = new Random(int.Parse(args[1]));
+
+            int n = 0;
+
+            int maxValue = 0;
+
+
+            while (n <= int.Parse(args[0]))
+            {
+                maxValue += random.Next(1, 6);
+                n++;
+            }
+
+            System.Console.WriteLine(maxValue);
+
         }
     }
 }
