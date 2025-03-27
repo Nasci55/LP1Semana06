44594224e2b6ddafe550442d60b362b3735f66a8
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-27 17:02:01 +0000

    Finish implementation of setting enemies names

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -12,14 +12,14 @@ namespace MyGame
             for (int numOfEnemies = 0; numOfEnemies < int.Parse(args[0]); numOfEnemies++)
             {
 
-                Console.WriteLine($"Nome do inimigo {numOfEnemies + 1}");
+                Console.Write($"Nome do inimigo {numOfEnemies + 1}: ");
                 enemies[numOfEnemies] = new Enemy(Console.ReadLine());
 
             }
 
             foreach (Enemy newEnemy in enemies)
             {
-                Console.WriteLine($"{newEnemy.GetName()}, {newEnemy.GetHealth()}, {newEnemy.GetShield()}");
+                Console.WriteLine($"{newEnemy.GetName()} {newEnemy.GetHealth()} {newEnemy.GetShield()}");
 
             }
 
