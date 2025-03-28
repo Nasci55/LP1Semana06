1ed9f9d45ef72d412acbdac98ae5a0cc288472ee
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-28 15:33:05 +0000

    Finish all exs

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -21,7 +21,7 @@ namespace MyGame
             {
                 newEnemy.TakeDamage(15);
                 newEnemy.PickupPowerUp(PowerUp.health, 5);
-                newEnemy.PickupPowerUp(PowerUp.shield, 500);
+                newEnemy.PickupPowerUp(PowerUp.shield, 7);
                 Console.WriteLine($"{newEnemy.GetName()} {newEnemy.GetHealth()} {newEnemy.GetShield()}");
 
             }
