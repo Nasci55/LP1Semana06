ba0366f072f762d576dcbc474396f84ebdcf749c
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-28 16:46:04 +0000

    Create MyRoguelike project

---
+++ MyRoguelike/Enemy.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    public class Enemy
+    {
+        private static int powerUpsGotten;
+        private string name;
+        private float health;
+        private float shield;
+
+
+        internal Enemy(string name)
+        {
+            this.name = SetName(name);
+            health = 100;
+            shield = 0;
+        }
+
+        static Enemy()
+        {
+            powerUpsGotten = 0;
+        }
+
+        public string GetName() => name;
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
+
+        public float GetHealth() => health;
+
+        public float GetShield() => shield;
+
+        public string SetName(string playerName)
+        {
+            string name = playerName;
+            int stringLenght = name.Length;
+
+
+            if (stringLenght > 8)
+            {
+                name = name.Substring(0, 8);
+            }
+            return name;
+        }
+
+        internal void PickupPowerUp(PowerUp powerUp, float num)
+        {
+            if (powerUp == PowerUp.health)
+            {
+                health += num;
+                if (health > 100)
+                    health = 100;
+
+            }
+            else if (powerUp == PowerUp.shield)
+            {
+                shield += num;
+                if (shield > 100)
+                    shield = 100;
+
+            }
+
+            powerUpsGotten += 1;
+        }
+
+        internal static int GetPowerUpsGotten() => powerUpsGotten;
+
+    }
+}
---
+++ MyRoguelike/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+
+namespace MyGame
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Enemy[] enemies = new Enemy[int.Parse(args[0])];
+
+            for (int numOfEnemies = 0; numOfEnemies < int.Parse(args[0]); numOfEnemies++)
+            {
+
+                Console.Write($"Nome do inimigo {numOfEnemies + 1}: ");
+                enemies[numOfEnemies] = new Enemy(Console.ReadLine());
+
+            }
+
+            foreach (Enemy newEnemy in enemies)
+            {
+                newEnemy.TakeDamage(15);
+                newEnemy.PickupPowerUp(PowerUp.health, 5);
+                newEnemy.PickupPowerUp(PowerUp.shield, 100);
+                Console.WriteLine($"{newEnemy.GetName()} {newEnemy.GetHealth()} {newEnemy.GetShield()}");
+
+            }
+            System.Console.WriteLine($"The number of PowerUps picked up was {Enemy.GetPowerUpsGotten()}");
+        }
+    }
+}
